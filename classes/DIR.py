cd4173141d38bb679823e0f5eba21380204dcab8
j0eTheRipper <j0eTheRipper@protonmail.com>
2021-07-14 16:54:22 +0300

    cleaned the DIR class and added extensions class

--- classes/DIR.py
+++ classes/DIR.py
@@ -1,34 +1,4 @@
-from json import load, dump
-from os import mkdir
-from os.path import isdir
+from .Extensions import Extensions
 
 
-class DIR:
-	def __init__(self, path, extensions, config_file):
-		self.path = path
-		self.extensions = extensions
-		self.config = f'{config_file}/config.json'
-		self.extensions_file = None
 
-	def get_existing_extensions(self):
-		try:
-			with open(self.config) as extensions:
-				self.extensions_file = load(extensions)
-		except FileNotFoundError:
-			self.extensions_file = dict()
-
-	def extensions_setup(self):
-		if self.path in self.extensions_file:
-			self.extensions_file[self.path].extend(self.extensions)
-		else:
-			self.extensions_file[self.path] = self.extensions
-
-		with open(self.config, 'w') as extensions:
-			dump(self.extensions_file, extensions)
-
-	def dir_setup(self):
-		self.get_existing_extensions()
-		self.extensions_setup()
-
-		if not isdir(self.path):
-			mkdir(self.path)
---
+++ classes/Extensions.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+from json import load, dump
+
+
+class Extensions:
+    def __init__(self, directory_for_extensions, extensions):
+        """add new extensions to the json file"""
+        self.__directory = directory_for_extensions
+        self.__extensions = set(extensions)
+        self.__extensions_file = {}
+        self._extensions_file_path = 'arrange.json'
+
+        self.__get_existing_extensions()
+        self.add_extensions(self.__extensions)
+        self.write_json_file()
+
+    def __get_existing_extensions(self):
+        """Get the extensions from the json file, if not created yet, Initialize json object"""
+        try:
+            with open(self._extensions_file_path, 'r') as extensions:
+                x = load(extensions)
+                self.__extensions_file = {directory: set(extension) for directory, extension in x.items()}
+        except FileNotFoundError:
+            pass
+
+    def add_extensions(self, extensions: set):
+        """Adds the extensions to the json object"""
+        self.__extensions_file = defaultdict(set, self.__extensions_file)
+        self.__extensions_file[self.__directory] = self.__extensions_file[self.__directory] | extensions
+
+    def remove_extensions(self, extensions: set):
+        extensions = set(extensions)
+        self.__extensions_file[self.__directory] = self.__extensions_file[self.__directory] - extensions
+
+    def write_json_file(self):
+        """Writes the json object to the json file"""
+
+        # turns all the extension sets into lists since json doesn't work with sets
+        extensions_list = {directory: list(extensions) for directory, extensions in self.__extensions_file.items()}
+
+        with open(self._extensions_file_path, 'w') as extensions:
+            dump(extensions_list, extensions)
