fa35e92f7bbcebfffcb04c502c3c6e29d2a056d9
j0eTheRipper <j0eTheRipper0010@gmail.com>
2021-10-31 22:44:35 +0300

    renamed the project

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 
 setup(
-        name='arrange',
+        name='arranger',
         version='1.0.0',
         description="moves each file to its appropriate directory based on the fil's extension.",
         author='j0eTheRipper',
         author_email='j0eTheRipper0010@gmail.com',
-        url='https://github.com/j0eTheRipper/arrange',
+        url='https://github.com/j0eTheRipper/arranger',
         scripts=['bin/arrange'],
         packages=['engine', 'engine.Extensions', 'engine.File', 'engine.DIR'],
-        package_dir={'': 'src', 'engine': 'src/engine'},
+        package_dir={'engine': 'src/engine'},
 )
